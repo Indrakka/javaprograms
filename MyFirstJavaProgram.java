d186b6e3bcc247a61a6aae0a9dddde23421853a2
Indrakka <96984424+Indrakka@users.noreply.github.com>
2022-10-18 14:34:14 +0530

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class MyFirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello My Name is Deeraj");
+		System.out.println("I am from TamilNadu");
+		System.out.println("I am studing in edubridge");
+
+	}
+
+}
