e3d7600816aed1edfa8ef8f7395c9440d5c2aa6c
Indrakka <96984424+Indrakka@users.noreply.github.com>
2022-10-18 14:32:12 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package edu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("My Name is Bharathi");
+		System.out.println("I am studying in Edubrideg");
+		System.out.println("Java Full Stack");
+
+	}
+
+}
